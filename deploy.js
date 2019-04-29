71e9f4bd661a7dacacf7dfa775e539ecff6e841f
robert french <rmf11699@gmail.com>
2019-04-29 08:32:41 -0500

    new ip

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.226.216.220',
+      host: '18.207.194.210',
       username: 'ubuntu',
       privateKey: 'todo-list.pem'
     })
